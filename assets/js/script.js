28c522e0c23dafd3ce226fffcb357ec09d3a6f48
Laura Johana Eugenio Rodriguez <78609488+johanaer@users.noreply.github.com>
2024-09-22 23:29:13 -0600

    acceder al nombreGasto  y al valorGasto en js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function clickBoton(){
+    let nombreGasto = document.getElementById('nombreGasto').value;
+    let valorGasto = document.getElementById('valorGasto').value;
+    alert('Click de usuario ');
+
+    console.log(nombreGasto);
+    console.log(valorGasto);
+    alert('click de usuario');
+}
