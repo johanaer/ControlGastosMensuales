cce57406e229a482e3163cf34997768d644f9c96
johana <johanarodriguez310@gmail.com>
2024-09-23 01:54:57 -0600

    proyecto final

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ let listaValoresGastos = [];
 function clickBoton(){
     let nombreGasto = document.getElementById('nombreGasto').value;
     let valorGasto = document.getElementById('valorGasto').value;
-    alert('Click de usuario ');
+    //alert('Click de usuario ');
 
     console.log(nombreGasto);
     console.log(valorGasto);
@@ -22,16 +22,42 @@ function clickBoton(){
     console.log(listaValoresGastos);
 
     //llamamos a la funcion es para mostrar los datos que fueron adicionadops por el usuario en la pantalla 
-    ActualizarLIstaGastos();
+    ActualizarListaGastos();
 }
 
-function ActualizarLIstaGastos(){
+function ActualizarListaGastos(){
+    // Actualizar en pantalla las cosas , capturar el html (ul) y traerlo a js 
+    const listaElementos = document.getElementById('listaDeGastos');
+    const totalElemetos = document.getElementById('totalGastos');
+
     //Agregamos variable para agregar una lista para los alimentos y los valores de cada alimento
     let htmlLista = '';
+    let totalGastos = 0;
     //recorrer el arreglo(
-    listaNombresGastos.forEach ((elemeto, posicion) => {
-        htmlLista += "<li>" + elemento + "</li>";
+    listaNombresGastos.forEach ((elemento, posicion) => {
+        const valorGasto = Number(listaValoresGastos[posicion]);
+        htmlLista += `<li>${elemento} - USD ${valorGasto.toFixed(2)} 
+        <button onclick="eliminarGasto(${posicion});">Eliminar gasto</button>
+        </li>`;
+        //calculamos el total de gastos
+        totalGastos += Number(valorGasto);
+        console.log(totalGastos);
 
     });
-    console.log(htmlLista);
+    listaElementos.innerHTML = htmlLista;    
+    totalElemetos.innerHTML = totalGastos.toFixed(2);
+    limpiar();
+}
+
+function limpiar(){
+    document.getElementById('nombreGasto').value ='';
+    document.getElementById('valorGasto').value = '';
+
+}
+
+function eliminarGasto(posicion){
+    listaNombresGastos.splice(posicion,1);
+    listaValoresGastos.splice(posicion, 1);
+    ActualizarListaGastos();
+
 }
